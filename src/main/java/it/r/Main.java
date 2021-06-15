da00d9ac9259f3f058bfd8aaaf8c27a23b26d817
Manuel Rascioni <mrascioni@hotels.com>
2021-06-15 14:35:22 +0200

    Initial commit

---
+++ src/main/java/it/r/Main.java
@@ -0,0 +1,10 @@
+package it.r;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("********");
+        System.out.println(Main.class.getConstructors().length);
+        System.out.println("********");
+    }
+}
